2b64bb7ff74a4e8929503ddb63d24ee067a4f38d
Sai Sashreek D <sai@5thdt.com>
2024-09-14 12:57:08 +0530

    Manifest Icon URL Updated

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,11 @@ export default defineConfig({
       injectRegister: "auto",
       registerType: "autoUpdate",
       includeAssets: [
-        "src/assets/images/meta/favicon.ico",
-        "src/assets/images/meta/apple-touch-icon.png",
-        "src/assets/images/meta/favicon-32x32.png",
-        "src/assets/images/meta/favicon-16x16.png",
-        "src/assets/images/meta/mask-icon.svg",
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "meta/favicon-32x32.png",
+        "meta/favicon-16x16.png",
+        "meta/mask-icon.svg",
       ],
       manifest: {
         name: "National Book Trust, India",
@@ -23,17 +23,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "src/assets/images/meta/android-chrome-192x192.png",
+            src: "android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "src/assets/images/meta/android-chrome-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "src/assets/images/meta/mask-icon.svg",
+            src: "mask-icon.svg",
             sizes: "156x165",
             type: "image/svg",
           },
